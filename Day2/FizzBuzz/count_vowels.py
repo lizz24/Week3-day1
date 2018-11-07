9d24523e5012c319749db10c8ecf0cba0cef95ca
lizz24 <39797444+lizz24@users.noreply.github.com>
2018-11-07 23:09:20 +0300

    Update count_vowels.py

--- Day2/FizzBuzz/count_vowels.py
+++ Day2/FizzBuzz/count_vowels.py
@@ -1,13 +1,16 @@
 def count_vowels(entry):
+#checking if the input variable is of datatype string    
     if not isinstance(entry, str):
         return'invalid input'
-
+#initializing vowels to a string
     vowels = 'aeiou'
+#making sure that the input string are lowercase    
     new_vow = entry.lower()
     original_string = set(new_vow)
+#evaluating the length of duplicates in the string    
     duplicate = len(new_vow)-len(original_string)
     newstring = ""
-
+#iterating through the string checking for only duplicate vowels
     for i in vowels:
         if i in original_string:
             newstring += str(i)
@@ -16,7 +19,7 @@ def count_vowels(entry):
 
 
 if __name__ == "__main__":
-
+# sample data for input string 
     print(count_vowels('ffggtree'))
 
 
