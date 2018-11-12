5b9dd19112d5f62890ccd567e7a868b72cd59037
lizz24 <39797444+lizz24@users.noreply.github.com>
2018-11-12 09:30:59 +0300

    Create account.py

---
+++ Day5/account.py
@@ -0,0 +1,47 @@
+class BankAccount(object):
+   
+#the initial balance is zero by default
+    def __init__(self) -> None:
+        self._balance = None
+
+    def error_if_closed(self) -> None:       
+#Raise a ValueError if the account is already closed.
+        
+        if self._balance is None:
+            raise ValueError("Cannot perform operations on a closed account!")
+
+    def get_balance(self) -> int:       
+#Get the curreny balance of the account.
+             
+        self.error_if_closed()
+        return self._balance
+
+    def open(self) -> None:
+        
+#Open the account and initialize the balance.
+        
+        self._balance = 0
+
+    def deposit(self, amount: int) -> None:
+#Deposit amount into account. Raises ValueError on negative amounts.
+        
+        self.error_if_closed()
+        if amount < 0:
+            raise ValueError("Cannot make negative deposits!")
+        self._balance += amount
+
+    def withdraw(self, amount):
+        
+#Withdraw amount from account. Raises ValueError on negative or overdraft withdrawls.
+        
+        self.error_if_closed()
+        if amount < 0:
+            raise ValueError("Cannot make negative withdrawls!")
+        elif amount > self._balance:
+            raise ValueError("Cannot make withdrawls exceeding balance!")
+        self._balance -= amount
+
+    def close(self):
+#Close the account.
+       
+        self._balance = None
