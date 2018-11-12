5cd47af6a195daece481be31871da5e8b2737027
lizz24 <39797444+lizz24@users.noreply.github.com>
2018-11-12 10:15:05 +0300

    Update dict.py

--- Day1/dict.py
+++ Day1/dict.py
@@ -1,7 +1,11 @@
 #python program run as a module
 #dictionary where the keys are numbers between 1 and 15 and the values are squares of the keys
 
+
+#Assigning variable d to a dictionary data type
 d=dict()
+# getting the range of the dictionary from 1 - 16
 for x in range(1,16):
+#squaring each number in range
     d[x]=x**2
     print(d)
